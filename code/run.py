6672b2d6b369df36c7d4d22645e7ccb7e0c96cd0
Jacky Sin <jackys97@connect.hku.hk>
2022-06-02 18:09:29 +0800

    some update

--- code/run.py
+++ code/run.py
@@ -3,8 +3,18 @@ import datetime
 import time
 import ssl
 from enum import Enum
+import random
 
+source = 'https://www.emqx.com/en/blog/how-to-use-mqtt-in-python'
+
+MQTT_HOST = 'sphku.com'
+MQTT_PORT = 8883
 MQTT_TOPIC = '#'
+MQTT_CLIENT_ID = f'python-mqtt-{random.randint(0, 1000)}'
+MQTT_USERNAME = 'device1'
+MQTT_PASSWORD = 'device1HKU'
+
+MQTT_CERT = '../mq.crt'
 
 def on_connect(mq, data, rc, _):
     global MQTT_TOPIC
@@ -19,15 +29,13 @@ def mqtt_publish(topic, msg, qos, retain):
     mqttClient.publish(topic, msg, qos, retain)
 
 def mqttInit():
-    mqttClient = mqtt.Client('device1')
-    mqttClient.username_pw_set('device1', 'device1HKU')
+    mqttClient = mqtt.Client(MQTT_CLIENT_ID)
+    mqttClient.username_pw_set(MQTT_USERNAME, MQTT_PASSWORD)
     mqttClient.on_connect = on_connect
     mqttClient.on_message = on_message
-    #
-    mqttClient.tls_set(ca_certs='../mq.crt', tls_version=ssl.PROTOCOL_TLSv1_2)
-    #
+    mqttClient.tls_set(ca_certs = MQTT_CERT, tls_version = ssl.PROTOCOL_TLSv1_2)
     mqttClient.tls_insecure_set(False)
-    mqttClient.connect(host='sphku.com',port=8883)
+    mqttClient.connect(host = MQTT_HOST, port = MQTT_PORT)
     mqttClient.loop_start()
     return mqttClient
 
@@ -35,9 +43,9 @@ try:
     # connect mqtt broker
     mqttClient = mqttInit()
 
-    # # forever loop
-    # while True:
-    #     time.sleep(2)
+    # forever loop
+    while True:
+        time.sleep(2)
 
 # ctrl-c
 except KeyboardInterrupt:
